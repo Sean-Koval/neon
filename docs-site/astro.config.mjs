f997f113b4e9047485b2006a73f2edea64294d8d
Sean Koval <seanmkoval@gmail.com>
2026-02-03 01:04:13 -0500

    Fix Pages base path

--- docs-site/astro.config.mjs
+++ docs-site/astro.config.mjs
@@ -5,7 +5,7 @@ const repoInfo = process.env.GITHUB_REPOSITORY?.split('/') ?? [];
 const owner = repoInfo[0];
 const repo = repoInfo[1];
 const site = owner && repo ? `https://${owner}.github.io/${repo}` : undefined;
-const base = repo ? `/${repo}` : '/';
+const base = repo ? `/${repo}/` : '/';
 
 // https://astro.build/config
 export default defineConfig({
